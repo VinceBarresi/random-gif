8ab1bed7ce70098f7aa77ccba0a378efd795aace
Vincent Barresi <vbarresi@amplify.com>
2015-07-31 17:44:03 -0400

    add random-gif.sh

---
+++ random_gif.sh
@@ -0,0 +1,33 @@
+function log {
+  echo "======== $1 ========"
+}
+
+function check_install_homebrew_mac {
+  log "Checking/Installing Homebrew"
+  if [ "$(which brew)" == "" ]; then
+    log "Installing Homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    log "Homebrew is already installed"
+  fi;
+}
+
+function install_jq {
+  if [ "$(find /usr/local/Cellar/jq)" == "" ]; then
+    log "Installing pq with Homebrew"
+    brew install jq
+  else
+    log "jq is already installed"
+  fi;
+}
+
+function get_gif {
+    url="http://api.giphy.com/v1/gifs/random?&api_key=dc6zaTOxFJmzC"
+    gif=$(curl $url | jq -r '.data.url')
+    export gif=$gif/fullscreen
+    open $gif
+}
+
+check_install_homebrew_mac
+install_jq
+get_gif
